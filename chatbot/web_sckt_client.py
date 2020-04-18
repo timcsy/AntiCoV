123bd21ea7231dd1125890befd96944a06582f6f
Chi-Yun-Lin <kvln268426@gmail.com>
2020-04-18 12:10:57 +0800

    Websocket built successfully

--- chatbot/web_sckt_client.py
+++ chatbot/web_sckt_client.py
@@ -1,3 +1,4 @@
+import requests
 import websocket
 
 #from websocket import create_connection
@@ -5,13 +6,6 @@ import websocket
 import json
 
 #import app as alarm_manager
-
-import config
-
-#from configparser import ConfigParser
-
-#config = ConfigParser()
-#config.read('app.config', encoding='UTF-8')
 
 def on_message(ws, message):
     print(ws)
@@ -25,17 +19,27 @@ def on_close(ws):
     print(ws)
     print("### closed ###")
 
+def on_open(ws):
+    req = {
+        "cmd": "temperature",
+        "device": "board",
+        "temperature": 1
+    }
+    msg_json = json.dumps(req)
+    ws.send(msg_json)
+    #result = ws.recv()
+    #print(result)
+    
 if __name__ == "__main__":
-    msg = {
+    access_key = {
         "username": "admin",
         "password": "AntiCoV"
     }
-    msg_json = json.dumps(msg)
+    r = requests.post('https://anticov.tew.tw/api/v1/login', data=access_key)
+    print(r.text)
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(config.webSocket['URL'], on_message=on_message, on_error=on_error, on_close=on_close)
+    ws = websocket.WebSocketApp('wss://anticov.tew.tw/data?token=' + r.text, on_message=on_message, on_error=on_error, on_close=on_close)
+    ws.on_open = on_open
     #ws = create_connection(config.webSocket['URL'])
-    ws.run_forever()
-    print("Try Connecting ...")
-    ws.send(msg_json)
-    result = ws.recv()
-    print(result)
+    ws.run_forever(ping_timeout=30)
+    print("Running ...")
