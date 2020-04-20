a47f46bc459d73216a8663145fa78582ab287d82
Chi-Yun-Lin <kvln268426@gmail.com>
2020-04-20 23:21:17 +0800

    Add something v2

--- chatbot/web_sckt_client.py
+++ chatbot/web_sckt_client.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     ws = websocket.WebSocketApp('wss://anticov.tew.tw/data?token=' + r.text, on_message=on_message, on_error=on_error, on_close=on_close)
     ws.on_open = on_open
     msg = ws.on_message
-    #print(msg)
+    print(msg)
     '''if msg['alarm'] == "bad":
         app.location_send_message("118", "1")
         app.broadcast_message("有人沒量體溫呢~~")
