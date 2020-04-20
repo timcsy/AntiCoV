8bb9ae24d851096cc8db87feed589e9134c29331
timcsy <timocsy@yahoo.com.tw>
2020-04-20 20:05:40 +0800

    edit API

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const Koa = require('koa')
+const cors = require('@koa/cors')
 const session = require('koa-session')
 const bodyParser = require('koa-bodyparser')
 const websockify = require('koa-websocket')
@@ -11,6 +12,7 @@ const path = require('path')
 const config = require('./config/server')
 
 const app = websockify(new Koa())
+app.use(cors())
 
 // sessions
 app.keys = config.SESSION_KEYS
